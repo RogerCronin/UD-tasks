4c09277147dc1bd7e7496075861856477524b383
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-02-27 17:24:29 -0500

    multiple choice question

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formMultipleChoiceQuestion">
+                <Form.Label>Answer</Form.Label>
+                <Form.Select
+                    value={answer}
+                    onChange={(e) => setAnswer(e.target.value)}
+                >
+                    {options.map((o) => (
+                        <option key={o} value={o}>
+                            {o}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
