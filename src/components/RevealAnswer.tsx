4cc0f59a7b39bad102adee76d62fcb78bfaa4f9f
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-02-27 11:29:04 -0500

    finish reveal answer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isRevealed, setIsRevealed] = useState<boolean>(false);
+
+    function toggleIsRevealed() {
+        setIsRevealed(!isRevealed);
+    }
+
+    return (
+        <div>
+            <Button onClick={() => toggleIsRevealed()}>Reveal Answer</Button>
+            {isRevealed ? <p>The answer is 42.</p> : ""}
+        </div>
+    );
 }
