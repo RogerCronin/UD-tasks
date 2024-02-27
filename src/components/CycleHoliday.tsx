d15ad80a137c534914b24762cc09981aa1b36cca
Roger <57421158+RogerCronin@users.noreply.github.com>
2024-02-27 12:16:31 -0500

    cycle holiday finish

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,45 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type HolidayEmoji = "🌳" | "🎂" | "🎃" | "🦃" | "🎅";
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    // I wanted to use a switch statement but eslint is like "no I don't want to"
+    function nextHolidayAlphabetically(current: HolidayEmoji): HolidayEmoji {
+        if (current === "🌳") return "🎂";
+        else if (current === "🎂") return "🎅";
+        else if (current === "🎅") return "🎃";
+        else if (current === "🎃") return "🦃";
+        else return "🌳";
+    }
+
+    function nextHolidayChronologically(current: HolidayEmoji): HolidayEmoji {
+        if (current === "🌳") return "🎂";
+        else if (current === "🎂") return "🎃";
+        else if (current === "🎃") return "🦃";
+        else if (current === "🦃") return "🎅";
+        else return "🌳";
+    }
+
+    const [currentEmoji, setCurrentEmoji] = useState<HolidayEmoji>("🌳");
+
+    return (
+        <div>
+            <Button
+                onClick={() =>
+                    setCurrentEmoji(nextHolidayAlphabetically(currentEmoji))
+                }
+            >
+                Advance by Alphabet
+            </Button>
+            <Button
+                onClick={() =>
+                    setCurrentEmoji(nextHolidayChronologically(currentEmoji))
+                }
+            >
+                Advance by Year
+            </Button>
+            <p>Holiday: {currentEmoji}</p>
+        </div>
+    );
 }
